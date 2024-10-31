e262c6ed9d9d8f1b2fc157ad3d2be47f9d2e7e79
Ardian Rama <ardian.rama@hotmail.se>
2024-10-31 09:45:49 +0100

    added buttons and labels

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -6,14 +6,26 @@ public class FifteenGame extends JFrame {
     JPanel boardPanel = new JPanel();
     JPanel northPanel = new JPanel();
 
+
+
     FifteenGame(){
         GameLogic gameLogic = new GameLogic();
 
         setLayout(new BorderLayout());
         add(boardPanel, BorderLayout.CENTER);
         boardPanel.setLayout(new GridLayout(4,4));
+        int index = 0;
+        for (int i = 0; i < 16; i++) {
+            boardPanel.add(new JButton("test"));
+        }
+
+        JButton NewGameButton = new JButton("New Game");
+        JLabel counter = new JLabel("0");
+
         add(northPanel, BorderLayout.NORTH);
         northPanel.setLayout(new FlowLayout());
+        northPanel.add(NewGameButton);
+        northPanel.add(counter);
 
         setVisible(true);
         setSize(400, 400);

19574f8146884b1478584f32f5264a83a32964b3
hyper <123260045+KieranCode94@users.noreply.github.com>
2024-10-31 09:21:14 +0100

    Gjort klart initBoard metod

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class GameLogic {
     private int size = 4;
     private int[][] board;
@@ -7,14 +12,25 @@ public class GameLogic {
     }
 
     private void initBoard() {
+        
+        List<Integer> listOfNumbers = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            listOfNumbers.add(i);
+        }
+        
+        Collections.shuffle(listOfNumbers);
+        
+        //TODO ifSolvable
+        
         board = new int[size][size];
-        int value = 0;
+        int index = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                board[i][j] = value;
-                System.out.println(value);
-                value++;
+                board[i][j] = listOfNumbers.get(index);
+                index++;
             }
         }
+
+        
     }
 }

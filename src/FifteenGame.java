658f9aaf6c57e0c8fbce306bf464eb943e280d2e
hyper <123260045+KieranCode94@users.noreply.github.com>
2024-10-31 11:02:04 +0100

    update UI efter flytt

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -1,32 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class FifteenGame extends JFrame {
 
     JPanel boardPanel = new JPanel();
     JPanel northPanel = new JPanel();
 
+    JButton[][] buttons;
+
 
 
     FifteenGame(){
         GameLogic gameLogic = new GameLogic();
         int[][] board = gameLogic.getBoard();
-
+        buttons = new JButton[board.length][board.length];
         setLayout(new BorderLayout());
         add(boardPanel, BorderLayout.CENTER);
         boardPanel.setLayout(new GridLayout(4,4));
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                JButton numberButton = new JButton();
+                buttons[i][j] = new JButton();
+                JButton tempButton = buttons[i][j];
                 if (board[i][j] == 0) {
-                    numberButton.setText("");
-                    numberButton.setEnabled(false);
+                    tempButton.setText("");
+                    tempButton.setEnabled(false);
                 } else {
-                    numberButton.setText(String.valueOf(board[i][j]));
+                    tempButton.setText(String.valueOf(board[i][j]));
                 }
-                numberButton.addActionListener(l -> gameLogic.performeMove(numberButton.getText()));
-                boardPanel.add(numberButton);
+                buttons[i][j].addActionListener(e -> {
+                    gameLogic.performeMove(tempButton.getText());
+                    updateUI(board);
+                });
+                boardPanel.add(buttons[i][j]);
             }
         }
 
@@ -42,6 +47,20 @@ public class FifteenGame extends JFrame {
         setSize(400, 400);
         //pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void updateUI(int[][] board) {
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons.length; j++) {
+                if (board[i][j] == 0) {
+                    buttons[i][j].setText("");
+                    buttons[i][j].setEnabled(false);
+                } else {
+                    buttons[i][j].setEnabled(true);
+                    buttons[i][j].setText(String.valueOf(board[i][j]));
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {

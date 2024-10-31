f4b410862c8f6af41be47cda06374a0b248bf8b7
Ardian Rama <ardian.rama@hotmail.se>
2024-10-31 09:37:56 +0100

    bas GUI

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -1,5 +1,27 @@
-public class FifteenGame {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+
+public class FifteenGame extends JFrame {
+
+    JPanel boardPanel = new JPanel();
+    JPanel northPanel = new JPanel();
+
+    FifteenGame(){
         GameLogic gameLogic = new GameLogic();
+
+        setLayout(new BorderLayout());
+        add(boardPanel, BorderLayout.CENTER);
+        boardPanel.setLayout(new GridLayout(4,4));
+        add(northPanel, BorderLayout.NORTH);
+        northPanel.setLayout(new FlowLayout());
+
+        setVisible(true);
+        setSize(400, 400);
+        //pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        FifteenGame game = new FifteenGame();
     }
 }
